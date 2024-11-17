678c5752cc31d1decd3daa00d71800d311fc8727
victor <victor.fouquet@gmail.com>
2024-11-17 18:45:56 +0100

    Refactor with BuilderGenerator class

--- src/BuilderGenerator/builder.generator.ts
+++ src/BuilderGenerator/builder.generator.ts
@@ -2,70 +2,83 @@ import * as ts from "typescript";
 import { writeFileSync, mkdirSync, readdirSync, existsSync } from "fs";
 import { BuilderFactory } from "./builder.factory";
 import { ImportFactory } from "./import.factory";
-import { EntityAstParser, Properties } from "./entity.ast-parser";
+import { EntityAstParser, ParsedEntity } from "./entity.ast-parser";
+import { BuilderGeneratorConfig } from "./builder-generator.config";
 
 
-//------------------------------------------------------------------------- Helpers
+export class BuilderGenerator {
+    private static readonly _targetFolder = `${__dirname}/${BuilderGeneratorConfig.TargetFolderPath}`;
+    private static readonly _sourceFolder = `${__dirname}/${BuilderGeneratorConfig.SourceFolderPath}`;
+    private static readonly _printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+    private static readonly _result = ts.createSourceFile('', '', ts.ScriptTarget.Latest, false, ts.ScriptKind.TS);
 
-function LogInfo(msg: string) {
-    console.log(`[INFO] ${msg}`)
-}
+    static generate() {
+        this.logInfo("Starting generating builders...\n");
 
-//------------------------------------------------------------------------- Source Generation
+        this.createOutputFolder();
+        const sourceContent = this.getSourceFolderContent();
 
+        this.logInfo(`Found ${sourceContent.length} entities to map in ./entities folder\n`)
 
+        for (let file of sourceContent) {
+            const parsedEntity = this.parseEntitySource(file)
 
+            this.logInfo(`Parsed entity ${parsedEntity.name} from ./entities/${file}`);
 
-function createBuilder(srcPath: string, className: string, properties: Properties, dependencies: [string, string][] = []) {
-    return ts.factory.createNodeArray([
-        ...ImportFactory.generate([[className, srcPath], ...dependencies]),
-        BuilderFactory.generate(className, properties)
-    ], false)
-}
+            const formattedImportPath = this.getFormattedSourcePath(file);
+            const formattedTargetPath = this.getFormattedTargetPath(file);
+            const builder = this.createBuilder(formattedImportPath, parsedEntity);
 
-function print() {
-    LogInfo("Starting generating builders...\n");
+            this.saveBuilder(formattedTargetPath, builder);
 
-    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-    const result = ts.createSourceFile('~/Documents/coding/ts-puzzles/tmp.ts', '', ts.ScriptTarget.Latest, false, ts.ScriptKind.TS)
-    const targetFolder = `${__dirname}/builders`;
-    const sourceFolder = `${__dirname}/entities`;
-
-    if (!existsSync(targetFolder)) {
-        mkdirSync(targetFolder);
+            this.logInfo(`Created mapped ${parsedEntity.name}Builder class in ${formattedTargetPath}\n`);
+        }
+        this.logInfo("Builders generation finished.")
     }
-    
-    const sourceContent = readdirSync(sourceFolder).filter(f => f.endsWith(".ts"));
 
-    LogInfo(`Found ${sourceContent.length} entities to map in ./entities folder\n`)
+    private static createOutputFolder(): void {
+        if (!existsSync(this._targetFolder)) {
+            mkdirSync(this._targetFolder);
+        }
+    }
 
-    for (let file of sourceContent) {
-        const parsedEntity = EntityAstParser.parse(`${sourceFolder}/${file}`);
+    private static getSourceFolderContent() {
+        return readdirSync(this._sourceFolder).filter(f => f.endsWith(".ts"));
+    }
 
-        LogInfo(`Parsed entity ${parsedEntity.name} from ./entities/${file}`);
+    private static parseEntitySource(fileName: string): ParsedEntity {
+        return EntityAstParser.parse(`${this._sourceFolder}/${fileName}`);
+    }
 
-        const formattedImportPath = `../entities/${file.slice(0, file.length - '.ts'.length)}`;
+    private static getFormattedSourcePath(fileName: string): string {
+        return `../entities/${fileName.slice(0, fileName.length - '.ts'.length)}`
+    }
 
-        const withoutSuffix = file.endsWith('.entity.ts') ?
-            file.slice(0, file.length - '.entity.ts'.length) :
-            file.slice(0, file.length - '.ts'.length);
-        const formattedOutputPath = `${targetFolder}/${withoutSuffix}.builder.ts`;
-    
+    private static getFormattedTargetPath(fileName: string): string {
+        const withoutSuffix = fileName.endsWith('.entity.ts') ?
+            fileName.slice(0, fileName.length - '.entity.ts'.length) :
+            fileName.slice(0, fileName.length - '.ts'.length);
+
+        return `${this._targetFolder}/${withoutSuffix}.builder.ts`;
+    }
+
+    private static saveBuilder(targetPath: string, builder: ts.NodeArray<ts.ImportDeclaration | ts.ClassDeclaration>) {
         writeFileSync(
-            formattedOutputPath,
-            printer.printList(ts.ListFormat.MultiLine,
-            createBuilder(
-                formattedImportPath,
-                parsedEntity.name,
-                parsedEntity.properties,
-                parsedEntity.imports
-            ),
-            result
+            targetPath,
+            this._printer.printList(ts.ListFormat.MultiLine,
+            builder,
+            this._result
         ), { flag: "w" });
-
-        LogInfo(`Created mapped ${parsedEntity.name}Builder class in ./builders/${withoutSuffix}.builder.ts\n`);
     }
-    LogInfo("Builders generation finished.")
-}
 
-print();
+    private static createBuilder(srcPath: string, parsedEntity: ParsedEntity) {
+        return ts.factory.createNodeArray([
+            ...ImportFactory.generate([[parsedEntity.name, srcPath], ...parsedEntity.imports ?? []]),
+            BuilderFactory.generate(parsedEntity.name, parsedEntity.properties)
+        ], false)
+    }
+
+    private static logInfo(msg: string) {
+        console.log(`[INFO] ${msg}`)
+    }
+}
